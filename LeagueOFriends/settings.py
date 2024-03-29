b3dd04c09ec065046d5ab2371d0570d2ec77aac9
Bobobrc <bobo.bogdan122@gmail.com>
2024-03-29 22:38:03 +0200

    test

--- LeagueOFriends/settings.py
+++ LeagueOFriends/settings.py
@@ -31,7 +31,7 @@ API_KEY = config('API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','leagueofriends.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'leagueofriends.onrender.com']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
